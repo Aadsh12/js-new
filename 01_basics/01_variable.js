71f7e4ba52155cefef9b849600bccb4549884480
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-01 07:08:21 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountnumber = 13245;
+let accountpass = "1345";
+var accountgamil= "aadishjain23";
+
+accountpass="473448";
+accountgamil="aadish455";
+
+// do not use var because of problem of block scope and fuctional scope 
+console.table([accountgamil,accountnumber,accountpass]);
