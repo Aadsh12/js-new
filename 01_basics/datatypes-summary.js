a17759925b9d04a9df650a4489f7ca09e4fcdc6d
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-04 18:49:30 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,16 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (primitve) , Heap(non-primitive)
+let mynme="aadish";
+let hello = "myname";
+
+hello="kaiseho";
+console.log(hello);
+
