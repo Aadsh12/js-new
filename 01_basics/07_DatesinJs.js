cfb22420fbc1c1f4d0bd7730c7bd545dacbbb0f3
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-05 16:45:04 +0000

    dates and time

---
+++ 01_basics/07_DatesinJs.js
@@ -0,0 +1,26 @@
+let mydate = new Date()
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+
+//let mycreateddate = new Date("15-06-2005"9)
+//console.log(mycreateddate.toLocaleString());
+
+
+
+let mycratedate=new Date("12-06-2024");
+//console.log(newdate.getDate());
+//nsole.log(newdate.getMonth());
+
+let exactdate = Date.now();
+//console.log(exactdate);
+
+//console.log(mycratedate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newda= new Date()
+
+console.log(newda.getMonth());
