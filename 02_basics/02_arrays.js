d66ed13ed60c0eb8df19ad8e17a7f2c83d7b4279
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-05 20:21:26 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+// const marval_heros=["thor","ironman","spiderman"]
+// const dc_heros=["superman","flash","batman"]
+
+// marval_heros.push(dc_heros);
+// console.log(marval_heros);
+
+// const all_heros=marval_heros.concat(dc_heros);
+// console.log(all_heros);
+
+// const anotherarray= [1,2,3,[4,5,6],7,[8,9,[5,6]]];
+
+// const newarray=anotherarray.flat(Infinity);
+
+//console.log(newarray);
+
+//console.log(Array.isArray("aadish"));
+console.log(Array.from("aadish"));
+//console.log(Array.from({name: "aadish"}));
+
+let score1=200;
+let score2=400;
+let score3=600;
+console.log(Array.of(score1,score2,score3));
+
+
