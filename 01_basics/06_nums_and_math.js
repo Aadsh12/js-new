a5fc66c4ed01d33b16890f1e8a84715d81ed7904
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-05 05:58:27 +0000

    nums and math calculations

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+let balance = new String(3456);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const othernumber= 4356.66
+
+//console.log(othernumber.toFixed(2));
+//console.log(othernumber.toPrecision(3));
+
+const humdreds=10000000;
+
+//console.log(humdreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++math+++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.7));
+// console.log(Math.floor(5.7));
+// console.log(Math.min(4,6,7,5,4));
+// console.log(Math.max(4,7,9,5,3));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
