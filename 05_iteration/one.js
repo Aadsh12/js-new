10acaa42c13135c7b827ab9854662feaeee4e191
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-09 20:11:22 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,41 @@
+// for (let i = 0;i <=10;i++) {
+//     console.log("multiplication",i);
+//     for(let j=0;j<=0;j++){
+ 
+//         console.log("anything",j);
+        
+//     }
+
+    
+    
+// }
+// for (let i = 0; i <=10; i++) {
+//     const element =i;
+//     if(element==5){
+//     console.log("lets go");
+//     }
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 0;i <=10;i++) {
+//     console.log("multiplication",i);
+//     for(let j=0;j<=0;j++){
+ 
+//         console.log("anything",j);
+        
+//     }
+// }
+// let myarray=[4,5,7,8,9]
+// console.log(myarray.length);
+
+
+for (let i = 0;i <=10;i++) {
+    if(i==5){
+ console.log("multiplication");
+continue;
+    }
+    console.log("anything");
+    
+}
