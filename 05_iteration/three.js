4a34954ebdec3c2cf1329969ff9b12aefe3a43aa
Aadish kumar jain <aadishkumarjain2@gmail.com>
2026-06-10 17:28:59 +0000

    forof lopp

---
+++ 05_iteration/three.js
@@ -0,0 +1,27 @@
+// for 
+
+const array=[4,5,6,7,8,9]
+
+// for (const print in array) {
+
+//     // console.log(print);
+// }
+
+for (const print of array) {
+//    console.log(print);
+    
+}
+
+const map = new Map()
+
+map.set('IN',"india")
+map.set('US',"united states of america")
+map.set('fr',"france")
+map.set('IN',"india")
+//console.log(map);
+for (const [key,value] of map) {
+    console.log(key,":-",value);
+    
+    
+}
+
